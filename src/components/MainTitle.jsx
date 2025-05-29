f59117e3303d50fdb7b3080c770d16b62548c732
GnzlzP <gnzlzp.dev@gmail.com>
2025-05-29 11:36:41 -0500

    mejorando mensaje home

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -17,7 +17,8 @@ export default function MainTitle() {
             <span className="block text-xl mt-4">Sin complicaciones!</span>
           </h1>
           <p className="text-[var(--shopify-dark)] text-xl mb-8">
-            ¿Vendes en tu local, mercados o por WhatsApp? Te ayudamos a crear tu
+            ¿Vendes en tu local, mercados o por WhatsApp?
+             Te ayudamos a crear tu
             tienda online profesional
           </p>
           <a
@@ -25,8 +26,18 @@ export default function MainTitle() {
             className="bg-[var(--shopify)] text-[#f3f3f3] px-8 py-3 rounded-full text-lg font-bold hover:bg-amber-50 hover:text-shopify transition duration-200">
             Quiero mi tienda online →
           </a>
-        </motion.div>
-        <span className="block text-[var(--shopify-dark)] text-3xl mb-8">Aprende como ⬇ </span>
+       
+    
+        <div className="mt-15 flex items-center" >
+          <span className="text-[var(--shopify-dark)] text-3xl space-x-2 font-bold">
+            Conoce la ruta para el éxito de tu tienda online!
+          </span>
+           <span className="ml-2 text-[var(--shopify-dark)]">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="-2 -2 24 24"><path fill="currentColor" d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10s-4.477 10-10 10m0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16m0-5.29l2.121-2.12a1 1 0 0 1 1.415 1.414l-2.829 2.828a1 1 0 0 1-1.414 0l-2.829-2.828a1 1 0 1 1 1.415-1.415zm0-5l2.121-2.12a1 1 0 0 1 1.415 1.414l-2.829 2.828a1 1 0 0 1-1.414 0L6.464 7.004A1 1 0 1 1 7.88 5.589L10 7.711z"/></svg>
+          </span>
+         
+        </div>
+      </motion.div>
       </div>
     </motion.section>
   );
